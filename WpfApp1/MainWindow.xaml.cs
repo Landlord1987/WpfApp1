4bd2d370bc0abe7da4cab02236662888335f9d88
Denis Krinitsyn <skylinekrin@gmail.com>
2022-12-11 20:21:25 +0500

    Add Button click

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -24,5 +24,47 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Button_Reg_Click(object sender, RoutedEventArgs e)
+        {
+            string login = textBoxLogin.Text.Trim();
+            string pass = passBox.Password.Trim();
+            string pass_2 = passBox_2.Password.Trim();
+            string email = textBoxEmail.Text.Trim().ToLower();
+
+            if (login.Length < 5)
+            {
+                textBoxLogin.ToolTip = "Это поле введено не корректно!";
+                textBoxLogin.Background = Brushes.LightPink;
+            }
+            else if (pass.Length < 5)
+            {
+                passBox.ToolTip = "Это поле введено не корректно!";
+                passBox.Background = Brushes.LightPink;
+            }
+            else if (pass != pass_2)
+            {
+                passBox_2.ToolTip = "Это поле введено не корректно!";
+                passBox_2.Background = Brushes.LightPink;
+            }
+            else if (email.Length < 5 || !email.Contains("@") || !email.Contains("."))
+            {
+                textBoxEmail.ToolTip = "Это поле введено не корректно!";
+                textBoxEmail.Background = Brushes.LightPink;
+            }
+            else
+            {
+                textBoxLogin.ToolTip = "";
+                textBoxLogin.Background = Brushes.Transparent;
+                passBox.ToolTip = "";
+                passBox.Background = Brushes.Transparent;
+                passBox_2.ToolTip = "";
+                passBox_2.Background = Brushes.Transparent;
+                textBoxEmail.ToolTip = "";
+                textBoxEmail.Background = Brushes.Transparent;
+
+                MessageBox.Show("Все хорошо");
+            }
+        }
     }
 }
